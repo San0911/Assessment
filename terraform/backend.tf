c9662fb6b4675b00f52a26c698d3c184eca12332
San0911 <161283747+San0911@users.noreply.github.com>
2024-12-20 00:42:10 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "san-tfstate-bucket"
-    key            = "terraform/state"
+    key            = "terraform"
     profile        = "User-1"
     region         = "us-west-2"
     dynamodb_table = "terraform-locks"
