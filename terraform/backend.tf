ea73bcffb680c036aa1d9832061fd970af58946c
San0911 <161283747+San0911@users.noreply.github.com>
2024-12-20 00:19:42 +0100

    Update backend.tf
    existing bucket to store state on creation

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "san-tfstate-bucket"
     key            = "terraform/state"
     profile        = "User-1"
     region         = "us-west-2"
