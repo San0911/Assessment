dc383113299f97340a2dba11b2c449d333f8a91e
San0911 <161283747+San0911@users.noreply.github.com>
2024-12-20 00:24:50 +0100

    Update main.tf
    removed creation of s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,20 +3,6 @@ provider "aws" {
   profile = "User-1"
 }
 
-# S3 bucket for state storage
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = var.s3_bucket_name
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
-}
 
 # DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_locks" {
