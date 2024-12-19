f6b23dfacb21995d660d05501ef139413abe87f3
San0911 <161283747+San0911@users.noreply.github.com>
2024-12-19 22:18:33 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "terraform/state"
+    profile        = "default"
     region         = "us-west-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
