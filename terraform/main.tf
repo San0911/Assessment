b8eea50960ff758a423809c804f3a3bcddfb8e31
San0911 <161283747+San0911@users.noreply.github.com>
2024-12-20 00:49:02 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 
 # PostgreSQL database
 resource "aws_db_instance" "postgres" {
-  allocated_storage    = 20
+  allocated_storage    = 5
   engine               = "postgres"
   engine_version       = "15.2"
   instance_class       = "db.t3.micro"
-  db_name                 = var.db_name
+  db_name              = var.db_name
   username             = var.db_user
   password             = var.db_password
   skip_final_snapshot  = true
